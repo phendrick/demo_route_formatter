46229ed4f85c875af0df59dbfe230ad097eca9f6
Paul Hendrick <paul.hendrick@gmail.com>
2022-06-16 22:31:48 +0100

    Handle all route rendering via a #draw(application_routes:, engines:)

--- lib/demo_route_formatter/formatters/emoji.rb
+++ lib/demo_route_formatter/formatters/emoji.rb
@@ -1,21 +1,27 @@
 class EmojiFormatter < ActionDispatch::Routing::ConsoleFormatter::Base
+  def draw(application_routes:, engines:)
+    compact = ARGV.collect { |arg| arg.gsub(/^-+/, "") }.include?("compact")
+
+    section(application_routes)
+
+    engines.each do |name, engine_routes|
+      add "\nRoutes for #{name}"
+
+      section(engine_routes)
+    end
+  end
+
+  private 
   def header(routes)
     name_width, verb_width, path_width = widths(routes)
-    "#{"Prefix".rjust(name_width)} #{"Verb"} Controller#Action"
+    add "#{"Prefix".rjust(name_width)} #{"Verb"} Controller#Action"
   end
 
   def section(routes)
     header_lengths = ["Prefix", "Verb", "Controller"].map(&:length)
     name_width, verb_width, path_width = widths(routes).zip(header_lengths).map(&:max)
 
-    compact = ARGV.collect { |arg| arg.gsub(/^-+/, "") }.include?("compact")
-
-    routes.reject do |r|
-      compact ? 
-        r[:path].match(/^\/rails\/(active_storage|action_mailbox|conductor\/action_mailbox)/) ||
-        r[:path].match(/^\/([^_]+)_historical_location/)
-      : false
-    end.map do |r|
+    routes.map do |r|
       vemoji = case r[:verb]
       when "GET"
         "🐶"
@@ -31,7 +37,7 @@ class EmojiFormatter < ActionDispatch::Routing::ConsoleFormatter::Base
         "🦁"
       end
 
-      "#{r[:name].rjust(name_width)}  #{vemoji}  #{r[:path]}"
+      add "#{r[:name].rjust(name_width)}  #{vemoji}  #{r[:path]}"
     end
   end
 end
--- lib/demo_route_formatter/tasks/super_routes/emojii.rake
+++ lib/demo_route_formatter/tasks/super_routes/emojii.rake
@@ -1,8 +1,4 @@
 # frozen_string_literal: true
 
 namespace :demo_route_formatter do
-  desc "Restart app by touching tmp/restart.txt"
-  task :emoji do
-    puts "☎️"
-  end
 end
